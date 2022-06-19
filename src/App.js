5718f47a0747cdbb13d82c0b62b1ae5af9decbc8
effiemartins <floramartins182@hotmail.com>
2022-06-19 19:25:39 -0300

    setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import { Component } from "react";
+
+class App extends Component {
+    
+    render(){
+
+        return(
+            <div class="App"></div>
+        )
+    }
+}
+
+export default App;
