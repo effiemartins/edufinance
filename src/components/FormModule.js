ad30cfe94fadbeb64e3864077b52630ba7a66bdf
effiemartins <floramartins182@hotmail.com>
2022-06-24 19:16:47 -0300

    form backend

--- src/components/FormModule.js
+++ src/components/FormModule.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export class FormModule extends React.Component {
     constructor(props) {
         super(props);
         
-        this.state = { userInput: '' };
-        
-        this.handleUserInput = this.handleUserInput.bind(this);
+        this.state = { name: '' };
+
     }
       
-    handleUserInput(e) {
-        this.setState({userInput: e.target.value});
+    handleChange = (e) => {
+      this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleSubmit = (e) => {
+      alert('A form was submitted: ' + JSON.stringify(this.state));
+      fetch('http://localhost:3307/store-data', {
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(this.state)
+      }).then(function(response) {
+        console.log(response)
+        return response.json();
+      });
+      e.preventDefault();
     }
 
     render() {
@@ -19,10 +32,12 @@ export class FormModule extends React.Component {
       return (
         <div className="form">
             <p>Entre na lista para ser avisado no lançamento do curso</p>
-            <input type="text" placeholder="Nome" value={this.state.userInput} onChange={this.handleUserInput} />
-            <input type="text" placeholder="E-mail" />
-            <input type="text" placeholder="Whatsapp (Opcional)" />
-            <input type="submit" value="Entrar na lista" className="submit" />
+              <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="Nome" name="name" value={this.state.value} onChange={this.handleChange} />
+              <input type="text" placeholder="E-mail" name="email" value={this.state.value} onChange={this.handleChange} />
+              <input type="text" placeholder="Whatsapp (Opcional)" name="whatsapp" value={this.state.value} onChange={this.handleChange} />
+              <input type="submit" value="Entrar na lista" className="submit" />
+            </form>
         </div>
       )
     }
